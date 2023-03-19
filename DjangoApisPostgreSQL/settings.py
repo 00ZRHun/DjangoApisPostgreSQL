f822f8bcd33409c367e7b3a1fc00201b136a7b93
00ZRHun <00zrhun@gmail.com>
2023-03-19 22:12:39 +0800

    set CORS_ORIGIN_ALLOW_ALL & add the host to CORS_ORIGIN_WHITELIST

--- DjangoApisPostgreSQL/settings.py
+++ DjangoApisPostgreSQL/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     # CORS
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +58,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081'
+)
 
 ROOT_URLCONF = 'DjangoApisPostgreSQL.urls'
 
