e92fadd0b71f35ec049b0265b74456b4a82db41a
00ZRHun <00zrhun@gmail.com>
2023-03-19 23:11:00 +0800

    create a new object

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 from django.http.response import JsonResponse
+from rest_framework import JSONParser
 from rest_framework import status
 
 from tutorials.models import Tutorial
+from tutorials.serializers import TutorialSerializers
 from rest_framework.decorators import api_view
 
 # Create your views here.
@@ -20,6 +22,19 @@ def tutorial_details(request, pk):
     #  GET / PUT / DELETE tutorial
 
 
-@api_view(['GET'])
-def tutorial_list_published(request):
-    # GET all published tutorials
+# @api_view(['GET'])
+# def tutorial_list_published(request):
+#     # GET all published tutorials
+
+# create a new object
+# create and save a new Tutorial
+@api_view(['GET', 'POST', 'DELETE'])
+def tutorial_list(request):
+    ...
+    elif request.method == 'POST':
+        tutorial_data = JSONParser().parse(request)
+        tutorial_serializer = TutorialSerializers(data=tutorial_data)
+        if (tutorial_serializer.is_valid()):
+            tutorial_serializer.save()
+            return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
