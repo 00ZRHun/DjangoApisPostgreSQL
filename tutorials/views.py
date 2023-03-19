ed721086471edeeaee3b74163e5b1631fdfc919a
00ZRHun <00zrhun@gmail.com>
2023-03-19 23:00:08 +0800

    write API Views

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+from django.http.response import JsonResponse
+from rest_framework import status
+
+from tutorials.models import Tutorial
+from rest_framework.decorators import api_view
+
 # Create your views here.
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def tutorial_details(request, pk):
+    # find tutorial by pk (id)
+    try:
+        tutorial = Tutorial.objects.get(pk=pk)
+    except Tutorial.DoesNotExist:
+        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    #  GET / PUT / DELETE tutorial
+
+
+@api_view(['GET'])
+def tutorial_list_published(request):
+    # GET all published tutorials
