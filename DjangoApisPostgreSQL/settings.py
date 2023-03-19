ba1672f30a896523c9f3d0f7e7a125beb2d85469
00ZRHun <00zrhun@gmail.com>
2023-03-19 22:30:16 +0800

    fix missing a comma and had a bracketed string like ('foo') rather than a one element tuple like ('foo',)

--- DjangoApisPostgreSQL/settings.py
+++ DjangoApisPostgreSQL/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8081'
+    'http://localhost:8081',
 )
 
 ROOT_URLCONF = 'DjangoApisPostgreSQL.urls'
